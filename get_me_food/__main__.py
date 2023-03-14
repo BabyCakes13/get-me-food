63e034fd104e1054d42073340725b9d309b9c152
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2023-03-14 20:29:29 +0200

    Added main, get_me_food and launch config

---
+++ get_me_food/__main__.py
@@ -0,0 +1,11 @@
+from get_me_food.get_me_food import GetMeFood
+
+
+def main() -> None:
+    """Main function."""
+    get_me_food = GetMeFood()
+    print(get_me_food)
+
+
+if __name__ == "__main__":
+    main()
---
+++ get_me_food/get_me_food.py
@@ -0,0 +1,11 @@
+"""Module which implements the logic of the application."""
+
+
+class GetMeFood:
+    """Entrypoint of the application."""
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "GetMeFood[]"
